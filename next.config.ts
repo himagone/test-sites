01ac8aad416cab39fb9b71cd1826bc0742c6251e
himagone <sunfgirl1975@icloud.com>
2025-03-25 19:44:11 +0900

    images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ const nextConfig = {
   output: "export",
   basePath: process.env.REPOSITORY_NAME ? `/${process.env.REPOSITORY_NAME}` : "",
   assetPrefix: process.env.REPOSITORY_NAME ? `/${process.env.REPOSITORY_NAME}/` : "",
+  images: {
+    unoptimized: true,
+    loader: 'custom',
+    loaderFile: `/${process.env.REPOSITORY_NAME}/`
+  },
 }
 
 module.exports = nextConfig

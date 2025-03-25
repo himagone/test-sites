735d4d557a1d260d24fe29d4dd10c8ed8aea77a6
himagone <sunfgirl1975@icloud.com>
2025-03-25 16:49:53 +0900

    aaa

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   basePath: `/${process.env.REPOSITORY_NAME}`,
-  images: {
-    unoptimized: true,
-  },
 }
 
 module.exports = nextConfig

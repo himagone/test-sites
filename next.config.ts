ddd27ca824b74c9a14547fca35c2f8338ae34a72
himagone <sunfgirl1975@icloud.com>
2025-03-25 15:42:09 +0900

    aaa

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: `/${process.env.REPOSITORY_NAME}/`,
+  basePath: `/${process.env.REPOSITORY_NAME}`,
   assetPrefix: `/${process.env.REPOSITORY_NAME}/`,
-  // 環境変数をクライアントサイドでも使えるようにする
   env: {
-    REPOSITORY_NAME: process.env.REPOSITORY_NAME || '',
-  },
-  images: {
-    unoptimized: true,
+    REPOSITORY_NAME: process.env.REPOSITORY_NAME,
   },
 }
 

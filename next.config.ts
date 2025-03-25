daceb63c4e6ca240ff57cf4fd7feebce448a7e08
himagone <sunfgirl1975@icloud.com>
2025-03-25 13:58:06 +0900

    aaa

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === 'production'
+const prefixPath = !isProd ? '/sub-derectory' : '';
 const nextConfig: NextConfig = {
+    assetPrefix: prefixPath,
+    basePath: prefixPath,
+    reactStrictMode: true,
     output: "export",
     experimental: {
         optimizePackageImports: ["@chakra-ui/react"],

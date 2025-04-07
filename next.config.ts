2a85c167b32bb3435766f4dd587fb9b91fde3e4e
himagone <sunfgirl1975@icloud.com>
2025-04-07 10:26:19 +0900

    nextconfigをしゅうせいしていない？

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.REPOSITORY_NAME ? `/${process.env.REPOSITORY_NAME}` : "",
-  assetPrefix: process.env.REPOSITORY_NAME ? `/${process.env.REPOSITORY_NAME}/` : "",
-  base: './',
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
 }
 
 module.exports = nextConfig

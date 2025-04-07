3370d3da1a5f7e703e617f325ba6ea147603639c
himagone <sunfgirl1975@icloud.com>
2025-04-07 10:32:46 +0900

    process.env.REPOSITORY_NAME

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  experimental: {
-    optimizePackageImports: ["@chakra-ui/react"],
-  },
+  basePath: process.env.IS_PUBLIC_REPO ? "" : process.env.REPOSITORY_NAME ? `/${process.env.REPOSITORY_NAME}` : "",
+  assetPrefix: process.env.IS_PUBLIC_REPO ? "" : process.env.REPOSITORY_NAME ? `/${process.env.REPOSITORY_NAME}/` : "",
+  base: './',
 }
 
 module.exports = nextConfig
